ba0fefaf2a54a9840e1376e8119a1521166c303f
kaishaun <89889353+kaishaun@users.noreply.github.com>
2021-09-07 20:50:39 -0400

    turtle homework

--- skyline.py
+++ skyline.py
@@ -1,31 +1,86 @@
 import turtle
 
-#this draws the rectangle
-def rectangle_draw():
-    turtle.color("gold")
-    turtle.fillcolor("DarkRed")
+Angle = 25
+
+ANGLE = 35
+
+angle = 120
+#this draws the rectangle/road.
+def road():
     turtle.begin_fill()
+
+    
+    turtle.left(180)
+    turtle.forward(530)
+    turtle.right(270)
+    turtle.forward(300)
+    turtle.setheading(90)
+    turtle.right(90)
+    turtle.forward(1100)
+    turtle.left(90)
+    turtle.forward(300)
+    turtle.home()
+
+    turtle.bgcolor("SkyBlue")
+    turtle.pensize(4)
+    turtle.color("White")
+    turtle.fillcolor("dimgray")
+    
+    
+    turtle.end_fill()
+#this is the square part of the house
+def house(ANGLE, angle, Angle):
+    turtle.begin_fill()
+
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.home()
+     
+
     
 
+    turtle.color("red")
+    turtle.fillcolor("FireBrick")
+
+    turtle.end_fill()
+    #this is the trangle to make the roof
+def house_roof(ANGLE, angle, Angle):
+    turtle.begin_fill()
+    turtle.left(90)
     turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(150)
-    turtle.setheading(90)
-    turtle.left(90)
-    turtle.forward(150)
-    turtle.forward(150)
-    turtle.left(90)
-    turtle.forward(150)
-    turtle.home()
+    turtle.right(ANGLE)
+    turtle.forward(100)
+    turtle.right(angle)
+    turtle.forward(100)
+    turtle.right(Angle)
+    turtle.forward(95)
+    turtle.right(90)
+    turtle.forward(100)
+
+
+    
+   
+
+    turtle.end_fill()
+
+
+
+
      
     
 
-turtle.begin_fill()
+
 
     
 
 def main ():
-    rectangle_draw()
+    road()
+    house(ANGLE, angle, Angle)
+    house_roof(ANGLE, angle, Angle)
     
       
 
